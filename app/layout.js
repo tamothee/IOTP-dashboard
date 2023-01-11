2518ef51d8423657c67e0512d0d0ae5d44161087
tamothee <timo9132@gmail.com>
2023-01-11 23:33:37 +0800

    foundation

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function RootLayout({children, ...props}) {
+  console.log("layout", { props }); // empty
+  return (
+    <html>
+      <head></head>
+      <body>
+        <SessionProvider session={props.session}>{children}</SessionProvider> 
+      </body>
+    </html>
+  );
+}
