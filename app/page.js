1528648e919f7d3d11d3abc189ed2c9494008aab
tamothee <timo9132@gmail.com>
2023-01-16 14:58:38 +0800

    added login page

--- app/page.js
+++ app/page.js
@@ -1,19 +1,6 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  );
+export default function HomePage() {
+  return(
+  <div>
+    home page
+  </div>)
 }
