5d566faaca6f79d5129611dca561c63c98f7e470
tamothee <timo9132@gmail.com>
2023-01-16 14:40:57 +0800

    auth0 test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import Auth0Provider from "next-auth/providers/auth0";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER
+    })
     // ...add more providers here
   ],
 }
