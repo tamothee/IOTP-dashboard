64c5052312133447c5eb0077f6477f48d209b163
tamothee <timo9132@gmail.com>
2023-01-21 00:51:33 +0800

    added skeleton code for mongo

--- app/page.js
+++ app/page.js
@@ -1,6 +1,80 @@
-export default function HomePage() {
-  return(
-  <div>
-    home page
-  </div>)
-}
+"use client"
+
+import React, { useState, useEffect } from "react";
+import * as Realm from "realm-web";
+const {
+  BSON: { ObjectId },
+} = Realm;
+const app = new Realm.App({ id: process.env.NEXT_PUBLIC_APP_ID });
+
+// Create the Application
+
+// Define the App component
+
+const HomePage = () => {
+  // Set state variables
+  const [user, setUser] = useState();
+  const [events, setEvents] = useState([]);
+  console.log('run')
+
+  // This useEffect hook will run only once when the page is loaded
+  useEffect(() => {
+    const login = async () => {
+      // Authenticate anonymously
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user); // Connect to the database
+
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("test").collection("testing"); // Everytime a change happens in the stream, add it to the list of events
+
+      for await (const change of collection.watch()) {
+        setEvents((events) => [...events, change]);
+      }
+    };
+    login();
+  }, []);
+
+  function write() {
+    const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+    const collection = mongodb.db("test").collection("testing"); // Everytime a change happens in the stream, add it to the list of events
+    collection.insertOne({
+      timestamp: new Date(),
+      value: 10000,
+    });
+  }
+
+  // Return the JSX that will generate HTML for the page
+  return (
+    <div className="App">
+      {!!user && (
+        <div className="App-header">
+          <h1>Connected as user ${user.id}</h1>
+          <div>
+            <p>Latest events:</p>
+            <table>
+              <thead>
+                <tr>
+                  <td>Operation</td>
+                  <td>Document Key</td>
+                  <td>Full Document</td>
+                </tr>
+              </thead>
+              <tbody>
+                {events.map((e, i) => (
+                  <tr key={i}>
+                    <td>{e.operationType}</td>
+                    <td>{e.documentKey._id.toString()}</td>
+                    <td>{JSON.stringify(e.fullDocument)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+      <button onClick={write}>write</button>
+    </div>
+  );
+};
+
+export default HomePage;
