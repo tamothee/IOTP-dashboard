92697bd0b03a6380d5dbdd60f548a59ca8a1d4ce
tamothee <timo9132@gmail.com>
2023-01-21 19:29:31 +0800

    fix error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 //import NextAuth from "next-auth";
-//import Auth0Provider from "next-auth/providers/auth0";
+import Auth0Provider from "next-auth/providers/auth0";
 
 // export const authOptions = {
 //   // Configure one or more authentication providers
@@ -15,21 +15,20 @@
 // };
 // export default NextAuth(authOptions);
 
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth from "next-auth";
 const nextOptions = {
   site: process.env.NEXTAUTH_URL,
   providers: [
-    Providers.Auth0({
+    Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       domain: process.env.AUTH0_ISSUER,
       audience: process.env.AUTH0_API_AUDIENCE,
-      scope: 'openid profile email',
-      protection: 'pkce',
+      scope: "openid profile email",
+      protection: "pkce",
       idToken: true,
       authorizationUrl: `https://${
-        process.env.AUTH0_ISSUER 
+        process.env.AUTH0_ISSUER
       }/authorize?response_type=code&audience=${encodeURI(
         process.env.AUTH0_API_AUDIENCE
       )}`,
