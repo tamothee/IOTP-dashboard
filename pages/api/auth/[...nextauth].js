1192c0081098ebe31f310ef1df859033c9326934
tamothee <timo9132@gmail.com>
2023-01-21 16:26:15 +0800

    revert back to normal

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,43 +1,15 @@
-import NextAuth from 'next-auth';
-import Auth0Provider from 'next-auth/providers/auth0';
+import NextAuth from "next-auth"
+import Auth0Provider from "next-auth/providers/auth0";
 
-export default NextAuth({
+export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-      audience: process.env.AUTH0_AUDIENCE,
-      idToken: true,
-      authorization: {
-        params: {
-          audience: encodeURI(process.env.AUTH0_AUDIENCE)
-        }
-      }
+      issuer: process.env.AUTH0_ISSUER
     })
+    // ...add more providers here
   ],
-  callbacks: {
-    session: async ({ session, token }) => {
-      if (token) {
-        session.user = token.user;
-        session.accessToken = token.accessToken;
-        session.error = token.error;
-      }
-      return session;
-    },
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-
-      return token;
-    }
-  },
-
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    jwt: true,
-    secret: process.env.NEXTAUTH_SECRET
-  }
-});
+}
+export default NextAuth(authOptions)
