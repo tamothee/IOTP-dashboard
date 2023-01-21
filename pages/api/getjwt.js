e3769511a4bcb8c581b6eacee6b6b1f83af14ab3
tamothee <timo9132@gmail.com>
2023-01-22 00:11:55 +0800

    log accesstoken

--- pages/api/getjwt.js
+++ pages/api/getjwt.js
@@ -6,6 +6,7 @@ let accessToken;
 export default async (req, res) => {
   const token = await getToken({ req, secret });
   accessToken = token.accessToken;
+  console.log(accessToken)
 
   res.status(200).json(accessToken);
 };
