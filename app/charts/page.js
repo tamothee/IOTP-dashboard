f518295f05cf9f8692269eb2651464ad413840d6
tamothee <timo9132@gmail.com>
2023-01-26 00:01:06 +0800

    added chart and fake data

--- app/charts/page.js
+++ app/charts/page.js
@@ -1,8 +1,65 @@
 'use client'
 
+import React from 'react';
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import Breadcrumbs from '@mui/material/Breadcrumbs';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 export default function Chart() {
   return (
@@ -15,6 +72,8 @@ export default function Chart() {
           Chart
         </Link>
       </Breadcrumbs>
+      <Line options={options} data={data} />
     </div>
   );
 }
+
