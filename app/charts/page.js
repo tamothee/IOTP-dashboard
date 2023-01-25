d1cde370f67f23aa5d65646f00c8b6f2e5c85fa1
tamothee <timo9132@gmail.com>
2023-01-25 23:36:45 +0800

    added chart page

---
+++ app/charts/page.js
@@ -0,0 +1,18 @@
+import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+
+export default function Chart() {
+  return (
+    <div>
+      <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: "10px" }}>
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+        <Link underline="hover" color="inherit" href="/charts">
+          Chart
+        </Link>
+      </Breadcrumbs>
+    </div>
+  );
+}
